11ccaf871d9f94b80478ae74aedd8a300967ffa4
Marcus Hammarberg <marcusoft.net@gmail.com>
2014-05-29 19:04:13 +0700

    Inital tests for infrastructure

--- ThingsILearned.md
+++ ThingsILearned.md
@@ -2,3 +2,4 @@ Things I learned writing this site
 ==================================
 
 - It's a good thing to write the README first. README driven development.
+- pushing empty apps to Heroku (or whatever) is great! Because then we can push test by test, all the way to prod. By pressing a key!
--- test/dbWrap.spec.js
+++ test/dbWrap.spec.js
@@ -1,4 +1,17 @@
+var should = require("should");
+var dbWrap = require("../lib/dbWrap.js");
+var config = require("../config")("local");
 
 describe("DB Wrapper", function () {
-	it("should report an test");
+	it("is a nice way of getting hold a database object", function (done) {
+		var db = dbWrap.getDatabase(config.mongoUrl);
+		should.exists(db);
+		done();
+	});
+	it("is a nice way of getting hold a generator friendly collection", function (done) {
+		var chunks = dbWrap.getCollection(config.mongoUrl, "chunks");
+		should.exists(chunks);
+		chunks.name.should.equal("chunks");
+		done();
+	});
 });
